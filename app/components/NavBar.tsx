c66ea9b0f323fbc1df5f5d12417fe552916fda03
Marcus <marcusmonteiro99@gmail.com>
2025-05-31 23:38:56 +0300

    rfc'd some stuff

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div>this is my navbar</div>
+  )
+}
---
+++ app/components/ScrollUp.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ScrollUp() {
+  return (
+    <div>ScrollUp</div>
+  )
+}
