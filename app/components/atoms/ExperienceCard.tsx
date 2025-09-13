19b5f897c0c278e3b73eed871f4a77a41e8b41c0
Marcus <marcusmonteiro99@gmail.com>
2025-09-13 19:39:55 +0300

    Experience base done

--- app/components/atoms/ExperienceCard.tsx
+++ app/components/atoms/ExperienceCard.tsx
@@ -1,5 +1,93 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { EXPERIENCES } from "../../config/constants";
+import {
+  motion,
+  MotionValue,
+  useScroll,
+  useTransform,
+  MapInputRange,
+} from "framer-motion";
+import { useRef } from "react";
 
-export default function ExperienceCard() {
-  return <div>ExperienceCard</div>;
+interface ExperienceCardProps {
+  i: number;
+  title: string;
+  description: string;
+  src: string;
+  link: string;
+  color: string;
+  range: MapInputRange;
+  targetScale: number;
+  progress: MotionValue<number>;
+}
+
+export default function ExperienceCard({
+  i,
+  title,
+  description,
+  src,
+  link,
+  color,
+  range,
+  targetScale,
+  progress,
+}: ExperienceCardProps) {
+  const container = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "start start"],
+  });
+  const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+  const scale = useTransform(progress, range, [1, targetScale]);
+
+  return (
+    <div
+      ref={container}
+      className="cardContainer h-screen flex items-center justify-center sticky top-0"
+    >
+      <motion.div
+        className="card flex flex-col relative h-[500px] w-[1000px] rounded-3xl p-12"
+        style={{
+          scale,
+          backgroundColor: color,
+          top: `calc(-5vh + ${i * 25}px)`,
+        }}
+      >
+        <h2 className="text-center m-0 text-xl">{title}</h2>
+        <div className="flex h-full mt-12 gap-12">
+          <div className="desc w-[40%] relative top-[10%]">
+            <p className="text-base first-letter:text-2xl">{description}</p>
+            <span className="flex items-center gap-1">
+              <Link
+                href={link}
+                target="_blank"
+                className="text-xs underline cursor-pointer"
+              >
+                See more
+              </Link>
+              <svg
+                width="22"
+                height="12"
+                viewBox="0 0 22 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989592 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75L21 6.75V5.25L0 5.25L0 6.75Z"
+                  fill="black"
+                />
+              </svg>
+            </span>
+          </div>
+          <div className="imgContainer relative w-[60%] h-full rounded-3xl overflow-hidden">
+            <motion.div style={{ scale: imageScale }} className="w-full h-full">
+              <Image fill src={src} alt="image" className="object-cover" />
+            </motion.div>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
--- app/components/sections/Experience.tsx
+++ app/components/sections/Experience.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import React from "react";
 import { useScroll } from "framer-motion";
 import { useRef } from "react";
+import { EXPERIENCES } from "@/app/config/constants";
+import ExperienceCard from "../atoms/ExperienceCard";
 
 export default function Experience() {
   const container = useRef<HTMLDivElement>(null);
@@ -10,8 +14,20 @@ export default function Experience() {
   });
 
   return (
-    <div className="min-h-screen grid grid-cols-12 gap-4 relative">
-      Experience
+    <div ref={container} className="min-h-screen relative">
+      {EXPERIENCES.map((experience, index) => {
+        const targetScale = 1 - (EXPERIENCES.length - index) * 0.05;
+        return (
+          <ExperienceCard
+            key={index}
+            i={index}
+            {...experience}
+            range={[index * 0.25, 1]}
+            targetScale={targetScale}
+            progress={scrollYProgress}
+          />
+        );
+      })}
     </div>
   );
 }
